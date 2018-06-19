e0b77f303ef9e36d9687378778fe206cc4959647
Devashish01 <malhotradevashish01@gmail.com>
2018-06-19 11:13:03 +0530

    assignment 9 finallyy

--- File1.java
+++ File1.java
@@ -8,31 +8,31 @@ public class File1 {
 
 	public static void main(String[] args) throws IOException {
 
-   File file=new File("D:\\Acdview\\xyz.txt");
+   File file=new File("D:\\Acdview\\abc.txt");
    FileReader  d1=new FileReader(file);
-   File files=new File("D:\\Acdview\\abc.txt");
+   File files=new File("D:\\Acdview\\xyz.txt");
    FileWriter d2=new FileWriter(files);
-   
-   try 
+
+   try
    {
-	   
+
 	   int ch;
 	   String q="";
-	   while((ch=d1.read())!=-1) 
+	   while((ch=d1.read())!=-1)
 	   {
-		   
+
 		   System.out.print((char)ch);
 	       q+=(char)ch;
-	       
+
 	   }
 	   char ab[]=q.toCharArray();
 	   int x=ab.length;
 	   for(int i=0;i<x;i++)
 	   {
-		   
+
 		   d2.write(ab[i]);
 	   }
-	   
+
    }
    catch(Exception e)
    {
