af41c37f051ea9b5e75bfb437385a63c34a64d6a
Devashish01 <malhotradevashish01@gmail.com>
2018-06-19 09:15:35 +0530

    assignment 9 finally

---
+++ File1.java
@@ -0,0 +1,48 @@
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class File1 {
+
+
+	public static void main(String[] args) throws IOException {
+
+   File file=new File("D:\\Acdview\\xyz.txt");
+   FileReader  d1=new FileReader(file);
+   File files=new File("D:\\Acdview\\abc.txt");
+   FileWriter d2=new FileWriter(files);
+   
+   try 
+   {
+	   
+	   int ch;
+	   String q="";
+	   while((ch=d1.read())!=-1) 
+	   {
+		   
+		   System.out.print((char)ch);
+	       q+=(char)ch;
+	       
+	   }
+	   char ab[]=q.toCharArray();
+	   int x=ab.length;
+	   for(int i=0;i<x;i++)
+	   {
+		   
+		   d2.write(ab[i]);
+	   }
+	   
+   }
+   catch(Exception e)
+   {
+	   System.out.println("Oho error!");
+   }
+   finally
+   {
+	   d2.close();
+	   d1.close();
+   }
+	}
+
+}
